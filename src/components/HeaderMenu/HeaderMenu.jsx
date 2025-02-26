2d38ecabd9d3f7b223343aa8597f9b7ad1827645
Caio Couto <coutocaio3@gmail.com>
2025-02-26 15:46:01 -0300

    Added function to close mobile menu when changing pages

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -1,17 +1,25 @@
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
+import { useAtom } from 'jotai';
+import { ShowBackdropAtom } from '../../jotai';
 
 export default function Headermenu() {
+  const [ showBackdrop, setShowBackdrop ] = useAtom(ShowBackdropAtom);
+
+  function handleLinkClick() {
+    setShowBackdrop(false);
+  }
+
   return (
     <ul className={ styles["header__menu"] }>
       <li className={ styles["header__menu-item"] }>
-        <Link to="/" className={ styles["header__menu-link"] }>Home</Link>
+        <Link to="/" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Home</Link>
       </li>
       <li className={ styles["header__menu-item"] }>
-        <Link to="/formatador" className={ styles["header__menu-link"] }>Formatador de Dietas</Link>
+        <Link to="/formatador" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Formatador de Dietas</Link>
       </li>
       <li className={ styles["header__menu-item"] }>
-        <Link to="/ExamReader" className={ styles["header__menu-link"] }>Leitor de Exames</Link>
+        <Link to="/ExamReader" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Leitor de Exames</Link>
       </li>
     </ul>
   );
