fbd2d611cf3df32e3a54f56a1b1f0b6972b084e7
Caio Couto <coutocaio3@gmail.com>
2025-03-03 14:55:46 -0300

    Added onClick and Ref props; Added styles

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -1,9 +1,10 @@
 import { Children } from "react";
+import styles from './styles.module.css';
 
-export default function Backdrop({ className, children }) {
+export default function Backdrop({ className, onClick, ref, children }) {
   
   return (
-    <div className={ className }>
+    <div className={ `${styles['backdrop']} ${className}` } onClick={ onClick } ref={ ref }>
       { children }
     </div>
   );
