334636c1d92d64d2569664f5a9b2b13a82f5ad68
Caio Couto <coutocaio3@gmail.com>
2025-03-03 14:58:21 -0300

    exported openMobileMenu and ExamReaderAdd states

--- src/jotai/index.js
+++ src/jotai/index.js
@@ -1,7 +1,11 @@
+import ExamReaderAddExamAtom from "./ExamReaderAddExamAtom";
 import IconSizeAtom from "./IconSizeAtom";
+import OpenMobileMenuAtom from "./OpenMobileMenuAtom";
 import ShowBackdropAtom from "./ShowBackdropAtom";
 
 export {
+  ExamReaderAddExamAtom,
   IconSizeAtom,
+  OpenMobileMenuAtom,
   ShowBackdropAtom
 };
