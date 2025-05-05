415c13c871886751e9f184873952d62118b930ed
Caio Couto <coutocaio3@gmail.com>
2025-05-05 10:14:28 -0300

    added set timeout

--- src/utils/showAlertComponent/showAlertComponent.js
+++ src/utils/showAlertComponent/showAlertComponent.js
@@ -1,3 +1,7 @@
 export default function showAlertComponent(message, type, show, setAlertFn) {
   setAlertFn({ message, type, show });
+
+  setTimeout(() => {
+    setAlertFn({ message: '', type: 'success', show: false });
+  }, 5000);
 }
