57eadeb58160c6079d14f1b34dc379f91ff5ce8a
Caio Couto <coutocaio3@gmail.com>
2025-05-07 07:12:02 -0300

    added Browser Router, Jotai Provider and AppWrapper on global level

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './normalize.css'
 import './index.css'
 import App from './App.jsx'
+import { 
+  BrowserRouter as Router
+} from 'react-router-dom';
+import { Provider } from 'jotai'
+import { AppWrapper } from './components'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Provider>
+        <AppWrapper>
+          <App />
+        </AppWrapper>
+      </Provider>
+    </Router>
   </StrictMode>,
 )
