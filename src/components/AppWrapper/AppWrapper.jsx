bf8bda6306e9a79785f9ae8c9d73db065a31f2bd
Caio Couto <coutocaio3@gmail.com>
2025-05-07 07:11:27 -0300

    implementd app wrapper to listen to global events and changes

---
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useAtom } from "jotai";
+import { OpenUserAuthenticationModalAtom } from "../../jotai";
+
+export default function AppWrapper({ children }) {
+  const { pathname } = useLocation();
+  const [_, setOpenUserAuthenticationModal] = useAtom(OpenUserAuthenticationModalAtom);
+
+  useEffect(() => {
+    const handler = () => {
+      const blockedPaths = ['/login'];
+
+      if (blockedPaths.includes(pathname)) {
+        console.log(`Bloqueando modal em rota ${pathname}`);
+        return;
+      }
+
+      setOpenUserAuthenticationModal(true);
+    };
+    
+    window.addEventListener("unauthorized-session", handler);
+
+    return () => {
+      window.removeEventListener("unauthorized-session", handler);
+    };
+  }, [pathname]); // importante: escuta mudanças de rota
+  
+
+  return children;
+}
