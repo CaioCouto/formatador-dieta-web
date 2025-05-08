d920028574f8bcc6897bf4a3717a8779850f1bfe
Caio Couto <coutocaio3@gmail.com>
2025-05-08 08:59:46 -0300

    exported new classes

--- src/classes/index.js
+++ src/classes/index.js
@@ -1,6 +1,8 @@
 import { EmptyExamNameError, EmptyExamResultsError, FileUploadError, FormFieldError } from "./Error/Error";
 import ExamResults from "./ExamResults";
 import Exams from "./Exams";
+import PatientResults from "./PatientResults";
+import Patients from "./Patients";
 import User from "./User";
 
 export {
@@ -10,5 +12,7 @@ export {
   ExamResults,
   FileUploadError,
   FormFieldError,
+  Patients,
+  PatientResults,
   User
 };
