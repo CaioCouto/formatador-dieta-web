5f6668a648e1e56d958e02dcabf8df359193270f
Caio Couto <coutocaio3@gmail.com>
2025-05-12 15:19:29 -0300

    addeing dropdown

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 import { useAtom } from 'jotai';
 import { OpenMobileMenuAtom } from '../../jotai';
+import { FaChevronDown } from 'react-icons/fa6';
 
 export default function Headermenu() {
   const [ openMobileMenu, setOpenMobileMenu ] = useAtom(OpenMobileMenuAtom);
@@ -11,16 +12,24 @@ export default function Headermenu() {
   }
 
   return (
-    <ul className={ styles["header__menu"] }>
-      <li className={ styles["header__menu-item"] }>
-        <Link to="/" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Home</Link>
-      </li>
-      <li className={ styles["header__menu-item"] }>
-        <Link to="/formatador" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Formatador de Dietas</Link>
-      </li>
-      <li className={ styles["header__menu-item"] }>
-        <Link to="/exams/list" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Leitor de Exames</Link>
-      </li>
-    </ul>
+    <div className={ styles["header__menu-wrapper"] }>
+      <ul className={ styles["header__menu"] }>
+        <li className={ styles["header__menu-item"] }>
+          <Link to="/" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Home</Link>
+        </li>
+        <li className={ styles["header__menu-item"] }>
+          <Link to="/formatador" className={ styles["header__menu-link"] } onClick={ handleLinkClick }>Formatador de Dietas</Link>
+        </li>
+        <li className={ `${styles["header__menu-item"]} ${styles["header__menu-dropdown"]}` }>
+          Listagem <FaChevronDown size={ 12 }/>
+          <div className={ styles["header__dropdown-menu"] }>
+            <Link to="/exams" className={ `${styles["header__menu-link"]} ${styles["header__dropdown-link"]}` } onClick={ handleLinkClick }>Exames</Link>
+            <Link to="/patients" className={ `${styles["header__menu-link"]} ${styles["header__dropdown-link"]}` } onClick={ handleLinkClick }>Pacientes</Link>
+          </div>
+        </li>
+      </ul>
+
+      <p className={ styles["header__menu-user"] }>logout</p>
+    </div>
   );
 }
